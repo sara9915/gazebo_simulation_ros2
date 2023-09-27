bb2e83eecf90cec16e84333f33cdcfda7100656f
Sara Federico <sara.federico99@hotmail.it>
2023-09-27 13:20:12 +0200

    First commit

---
+++ launch/robot_gazebo.launch.py
@@ -0,0 +1,84 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import SetEnvironmentVariable
+from launch.actions import TimerAction
+import xacro
+
+
+def generate_launch_description():
+
+    # extracting the robot deffinition from the xacro file
+    xacro_file = os.path.join(get_package_share_directory('gazebo_simulation'), 'description','manipulator.urdf.xacro')
+    robot_description_content = xacro.process_file(xacro_file).toxml()
+
+    rviz_absolute_path = os.path.join(get_package_share_directory('gazebo_simulation'),'rviz','visualize.rviz')
+
+    xacro_robot = os.path.join(get_package_share_directory('yaskawa_description'), 'robots','sia5f_arm.xacro')
+
+    yaml_file = os.path.join(get_package_share_directory('gazebo_simulation'),'config','controllers.yaml')
+
+    # robot state publisher node
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description_content}]
+    )
+
+    node_joint_state_publisher = Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui'
+    )
+
+    # Rviz2 node
+    node_rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_absolute_path]
+    )
+
+    # Gazebo launch file
+    launch_gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+        #launch_arguments={'world': world_path}.items()
+    )
+
+    # entity spawn node (to spawn the robot from the /robot_description topic)
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'my_bot'],
+                        output='screen')
+    
+    # spawning the joint broadcaster
+    spawn_broadcaster = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster","--controller-manager", "/controller_manager"],
+        output="screen",
+    )
+
+    spawn_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_trajectory_controller","-c", "/controller_manager"],
+        output="screen",
+    )
+
+
+    # Run the nodes
+    return LaunchDescription([
+        #SetEnvironmentVariable(name='GAZEBO_MODEL_PATH', value=model_path),
+        node_robot_state_publisher,
+        node_joint_state_publisher,
+        launch_gazebo,
+        spawn_entity,
+        node_rviz,
+        spawn_broadcaster,
+        spawn_controller
+    ])
